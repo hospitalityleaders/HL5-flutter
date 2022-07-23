f7acc56b8d23bf17c3415a2b566809042d98111a
Kalimullah <Kalimullahdev@gmail.com>
2022-07-23 07:29:35 +0500

    Built web
    Branch: hl-profile-desktop-overview
    Commit: 1c5a17aa87eb57d51ed20148ea559eb350370880 (dirty)
    
    package:peanut 4.2.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "793be19e5cd8b6c2aa8528aad64fff27",
-"index.html": "baf1796e64ee74c2205bf1e95bfae553",
-"/": "baf1796e64ee74c2205bf1e95bfae553",
-"main.dart.js": "947de8959fa9cf55047adc4cf4e26d0d",
+"index.html": "19ac1779fb7a0ac3e2265d1ea426a504",
+"/": "19ac1779fb7a0ac3e2265d1ea426a504",
+"main.dart.js": "bcbc7ce89e6f59626ea5fb5b7eac156c",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
